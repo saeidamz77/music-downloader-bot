965e73f87d917d0fa0bec06e635992c721eb7a3a
saeidamz77 <yddysya4hh@atomicmail.io>
2026-09-16 18:29:25 +0330

    Update bot token retrieval method

--- bot.py
+++ bot.py
@@ -19,7 +19,7 @@ import yt_dlp
 from shazamio import Shazam
 
 # ==================== تنظیمات سرور و مدیریت ====================
-TOKEN = os.environ.get("BOT_TOKEN", "8924723567:AAH1ag1Ccc_t8DTy6u6ayw1kM8I9SWziuBY")
+TOKEN = os.environ.get("8924723567:AAH1ag1Ccc_t8DTy6u6ayw1kM8I9SWziuBY", "توکن_ربات")
 BOT_USERNAME = os.environ.get("BOT_USERNAME", "@Instadlmusicbot")
 SUPPORT_ID = os.environ.get("SUPPORT_ID", "@saeed_mz77")
 CHANNEL_ID = os.environ.get("CHANNEL_ID", "@ainewss2026")
